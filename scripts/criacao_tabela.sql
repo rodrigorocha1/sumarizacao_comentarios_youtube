34b0f2f3dfdc3c03a1aea1e54767d34fa3e0e0f7
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-11-21 22:25:40 -0300

    criação tabela

--- scripts/criacao_tabela.sql
+++ scripts/criacao_tabela.sql
@@ -19,6 +19,7 @@ CREATE table comentarios (
 	id_video string,
 	usuario string,
 	comentario string,
+	comentario_atualizado,
 	data_publicacao string,
 	data_atualizacao string,
 	FOREIGN KEY(id_video) REFERENCES videos (id_video)
@@ -29,7 +30,7 @@ CREATE table resposta_comentarios (
 	id_resposta_comentario string PRIMARY KEY,
 	id_comentario string,
 	usuario string,
-	comentario string,
+	resposta_comentario string,
 	data_publicacao string,
 	data_atualizacao string,
 	FOREIGN KEY(id_comentario) REFERENCES comentarios (id_comentario)
